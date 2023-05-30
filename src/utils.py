00420aafbe0fa2c9732ccab3338930f1739cb7ba
Abhiinv <abhiinv24@gmail.com>
2023-05-30 12:09:20 +0530

    Add Hyperparameter Tuning

--- src/utils.py
+++ src/utils.py
@@ -6,40 +6,48 @@ import pandas as pd
 import dill
 
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 
-def save_object(file_path,obj):
+
+def save_object(file_path, obj):
     try:
-        dir_path=os.path.dirname(file_path)
+        dir_path = os.path.dirname(file_path)
 
-        os.makedirs(dir_path,exist_ok=True)
+        os.makedirs(dir_path, exist_ok=True)
 
-        with open(file_path,'wb') as f:
-            dill.dump(obj,f)
+        with open(file_path, 'wb') as f:
+            dill.dump(obj, f)
 
     except Exception as e:
-        raise CustomException(e,sys)
-    
-def evaluate_models(X_train,y_train,X_test,y_test,models):
+        raise CustomException(e, sys)
+
+
+def evaluate_models(X_train, y_train, X_test, y_test, models, params):
     try:
-        report={}
+        report = {}
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            param = params[list(models.keys())[i]]
 
-            model.fit(X_train,y_train)
+            grid_search = GridSearchCV(model, param, cv=3)
+            grid_search.fit(X_train, y_train)
 
-            y_train_pred=model.predict(X_train)
+            model.set_params(**grid_search.best_params_)
+            model.fit(X_train, y_train)
 
-            y_test_pred=model.predict(X_test)
+            y_train_pred = model.predict(X_train)
 
-            train_model_score=r2_score(y_train,y_train_pred)
+            y_test_pred = model.predict(X_test)
 
-            test_model_score=r2_score(y_test,y_test_pred)
+            train_model_score = r2_score(y_train, y_train_pred)
 
-            report[list(models.keys())[i]]=test_model_score
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
 
         return report
-    
+
     except Exception as e:
-        raise CustomException(e,sys)
+        raise CustomException(e, sys)
